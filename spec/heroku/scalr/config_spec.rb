f11ade96375160b7069cdaae9f1509da40999ee4
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2013-05-08 18:22:37 +0100

    Improve config spec

--- spec/heroku/scalr/config_spec.rb
+++ spec/heroku/scalr/config_spec.rb
@@ -4,10 +4,14 @@ describe Heroku::Scalr::Config do
 
   subject { described_class.new(fixture_path("config_a.rb")) }
 
-  its(:apps)     { should be_a(Array) }
-  its(:defaults) { should eq({:api_key=>"API_KEY"}) }
+  its(:apps)     { should be_instance_of(Array) }
+  its(:apps)     { should have(2).items }
+  its(:defaults) { should eq(api_key: "API_KEY") }
 
-  it "has to contain App object" do
-    subject.apps[0].should be_instance_of(Heroku::Scalr::App)
+  it 'should merge defaults into app configurations' do
+    app = subject.apps.first
+    app.should be_instance_of(Heroku::Scalr::App)
+    app.api.instance_variable_get(:@api_key).should == "API_KEY"
   end
+
 end
