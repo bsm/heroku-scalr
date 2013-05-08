3c8bdcf2aa764e2be1b685dfd6077bd22f314220
eggya <eggyachiquita@yahoo.com>
2013-05-08 18:09:23 +0100

    spec for config

--- spec/heroku/scalr/config_spec.rb
+++ spec/heroku/scalr/config_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe Heroku::Scalr::Config do
+
+  subject { described_class.new(fixture_path("config_a.rb")) }
+
+  its(:apps)     { should be_a(Array) }
+  its(:defaults) { should eq({:api_key=>"API_KEY"}) }
+
+  it "has to contain App object" do
+    subject.apps[0].should be_instance_of(Heroku::Scalr::App)
+  end
 end
